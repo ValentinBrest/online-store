0451f8f4019f42e0f2b06d7613047bf76b43fbb6
Valiantsin Kanapatski <fixator03@yandex.ru>
2022-07-14 20:19:07 +0300

    docs: add Filters component

---
+++ online-store/src/layout/Filters/Filters.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Button from '../../components/Button/Button';
+import FilterByRanges from '../../components/FilterByRanges/FilterByRanges';
+import FilterByValues from '../../components/FilterByValues/FilterByValues';
+import Search from '../../components/Search/Search';
+import Sort from '../../components/Sort/Sort';
+import cl  from './Filters.module.css';
+
+const Filters = ({ setProducts, date }) => {
+    return (
+        <div className={cl.filters}>
+            <Button className={cl.button}>Сброс фильтров</Button>
+            <Button className={cl.button}>Сброс настроек</Button>
+            <FilterByValues />
+            <FilterByRanges />
+            <Search setProducts={setProducts} date={date} />
+            <Sort setProducts={setProducts} date={date} />
+        </div>
+    );
+};
+
+export default Filters;
---
+++ online-store/src/layout/Filters/filtersType.js
@@ -0,0 +1,51 @@
+export const filterProducer = [
+    {
+        id: 'apple',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg',
+    },
+    {
+        id: 'samsung',
+        img: 'https://i.pinimg.com/736x/e5/cb/66/e5cb66fe655e53d607210abe8015a7b0.jpg',
+    },
+    {
+        id: 'xiaomi',
+        img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ae/Xiaomi_logo_%282021-%29.svg/2048px-Xiaomi_logo_%282021-%29.svg.png',
+    },
+];
+
+export const filterCameras = [
+    {
+        id: 1,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        quantity: 2,
+    },
+    {
+        id: 3,
+        quantity: 3,
+    },
+];
+
+export const filterColor = [
+    {
+        id: 'white',
+        color: 'white',
+    },
+    {
+        id: "yellow",
+        color: "yellow",
+    },
+    {
+        id: 'red',
+        color: 'red',
+    },
+];
+
+export const filterPopular = [
+    {
+        id: 'grey',
+        color: 'grey',
+    },
+];
