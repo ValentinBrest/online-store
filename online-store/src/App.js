2f33582c9e4a57357d803bc2cd9241d56073100a
Valiantsin Kanapatski <fixator03@yandex.ru>
2022-07-14 20:21:46 +0300

    docs:change App component

--- online-store/src/App.js
+++ online-store/src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import Filters from './layout/Filters/Filters';
+import Footer from './layout/Footer/Footer';
+import Header from './layout/Header/Header';
+import ProductsList from './layout/ProductsList/ProductsList';
+import { useState } from 'react';
+import { date } from './products';
 
 function App() {
+    const [quantityProducts, setQuantityProducts] = useState(0);
+    const [products, setProducts] = useState(date);
 
     return (
         <div className="App">
-            
+            <Header quantityProducts={quantityProducts} />
+            <div className="main">
+                <div className="container">
+                    <div className="main__wrap">
+                        <Filters date={date} setProducts={setProducts} />
+                        <ProductsList
+                            setQuantityProducts={setQuantityProducts}
+                            products={products}
+                        />
+                    </div>
+                </div>
+            </div>
+            <Footer />
         </div>
     );
 }
