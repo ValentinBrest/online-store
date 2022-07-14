abb75b25aa60adf21d8138050c338276b6c1f403
Valiantsin Kanapatski <fixator03@yandex.ru>
2022-07-14 20:16:23 +0300

    docs: add Header component

---
+++ online-store/src/layout/Header/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import cl from './Header.module.css'
+
+const Header = ({ quantityProducts }) => {
+    return (
+        <div className={cl.header}>
+            <div className="container">
+                <div className={cl.header__wrap}>
+                    <h1 className={cl.header__logo}>
+                        <a href="#">Online Shop</a>
+                    </h1>
+                    <div className={cl.header__kart}>
+                        <div className={cl.header__count}>{quantityProducts}</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
