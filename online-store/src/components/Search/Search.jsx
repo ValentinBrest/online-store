f36bfdf13a3f6f43c0cf510fd02e95ac6c5fa264
Valiantsin Kanapatski <fixator03@yandex.ru>
2022-07-14 20:18:25 +0300

    docs: add Search component

---
+++ online-store/src/components/Search/Search.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import cl from './Search.module.css';
+import { useState } from 'react';
+
+const Search = ({ date, setProducts }) => {
+    return (
+        <div className={cl.search}>
+            <div className={cl.title}>Поиск</div>
+            <input
+                type="search"
+                placeholder="Введите текст"
+                id="search"
+                class={cl.input}
+                onChange={(event) => setProducts(
+                        date.filter((item) => {
+                            return item.title
+                                .toLowerCase()
+                                .includes(event.target.value.toLowerCase());
+                        })
+                    )
+                }
+            />
+        </div>
+    );
+};
+
+export default Search;
