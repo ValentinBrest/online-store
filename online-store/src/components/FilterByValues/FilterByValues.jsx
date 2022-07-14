528e0fd491c0027f7b31f6f66934391cf84fd5a0
Valiantsin Kanapatski <fixator03@yandex.ru>
2022-07-14 20:17:39 +0300

    docs: add FilterByValues component

---
+++ online-store/src/components/FilterByValues/FilterByValues.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import Checkbox from '../Checkbox/Checkbox';
+import cl from './FilterByValues.module.css';
+import {
+    filterProducer,
+    filterCameras,
+    filterColor,
+    filterPopular,
+} from './../../layout/Filters/filtersType';
+
+const FilterByValues = () => {
+    const label = { inputProps: { 'aria-label': 'Switch demo' } };
+	return (
+        <div className={cl.filter}>
+            <div className={cl.title}>Фильты по значению</div>
+            <div className={cl.filter__wrap}>
+                <div className={cl.producer}>
+                    <span>Производитель:</span>
+                    {filterProducer.map((_, i) => {
+                        return <Checkbox id={filterProducer[i].id} img={filterProducer[i].img} />;
+                    })}
+                </div>
+                <div className={cl.cameras}>
+                    <span>Количество камер:</span>
+                    {filterProducer.map((_, i) => {
+                        return (
+                            <Checkbox
+                                id={filterCameras[i].id}
+                                quantity={filterCameras[i].quantity}
+                            />
+                        );
+                    })}
+                </div>
+                <div className={cl.color}>
+                    <span>Цвет:</span>
+                    {filterProducer.map((_, i) => {
+                        return <Checkbox id={filterColor[i].id} color={filterColor[i].color} />;
+                    })}
+                </div>
+                <div className={cl.popular}>
+                    <span>Только популярные:</span>
+                    {<Checkbox id={filterPopular[0].id} color={filterPopular[0].id} />}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FilterByValues;
